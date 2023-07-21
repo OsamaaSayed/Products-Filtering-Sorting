4260e3b4b57cbaa1e76973737e44a2c6e25d4835
Osama Sayed <osamasayedmuhammad@gmail.com>
2023-07-21 23:22:11 +0300

    🩹 Small enhancement

--- src/components/FilterProducts/index.jsx
+++ src/components/FilterProducts/index.jsx
@@ -18,7 +18,7 @@ const FilterProducts = ({
 
   return (
     <div className="flex flex-col gap-5 2xs:justify-center 2xs:items-center">
-      <h1 className="underline font-semibold text-2xl">Filter</h1>
+      <h1 className="underline font-semibold text-2xl w-full 2xs:text-left">Filter</h1>
 
       <div>
         <h2 className="mb-3 text-lg font-medium">Category</h2>
